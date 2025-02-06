76890dae168016db63a27405ab861a91bfa5a979
Grigory-Vladimiro <grigori.vladimiro@gmail.com>
2025-02-06 11:39:12 +0100

    Ghost Class

--- src/main/java/model/Ghost.java
+++ src/main/java/model/Ghost.java
@@ -1,5 +1,11 @@
 package model;
 
-public class Ghost {
+import java.time.LocalDate;
 
+public class Ghost {
+private String nombre;
+    private ClassGhost clase;
+    private DangerLevel nivelPeligro;
+    private String ability;
+    private LocalDate dateBusted;
 }
