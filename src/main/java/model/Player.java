e8a938f6c1e75e8e958bec0a9ae85c51a07f7b94
Grigory-Vladimiro <grigori.vladimiro@gmail.com>
2025-02-06 14:16:36 +0100

    player_update

--- src/main/java/model/Player.java
+++ src/main/java/model/Player.java
@@ -1,5 +1,44 @@
 package model;
 
-public class Player {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Player {
+    private List<Ghost> capturedGhosts;
+
+    public Player() {
+        this.capturedGhosts = new ArrayList<>();
+    }
+
+    public void captureGhost(Ghost ghost) {
+        capturedGhosts.add(ghost);
+    }
+
+    public void releaseGhost(Ghost ghost) {
+        capturedGhosts.remove(ghost);
+    }
+
+    public List<Ghost> filterByClass(Ghost.GhostClass ghostClass) {
+        List<Ghost> filtered = new ArrayList<>();
+        for (Ghost ghost : capturedGhosts) {
+            if (ghost.getGhostClass() == ghostClass) {
+                filtered.add(ghost);
+            }
+        }
+        return filtered;
+    }
+
+    public List<Ghost> filterByMonth(int month) {
+        List<Ghost> filtered = new ArrayList<>();
+        for (Ghost ghost : capturedGhosts) {
+            if (ghost.getCaptureDate().getMonthValue() == month) {
+                filtered.add(ghost);
+            }
+        }
+        return filtered;
+    }
+
+    public List<Ghost> getCapturedGhosts() {
+        return capturedGhosts;
+    }
 }
