3ea328e051b8b91db3a6a9c0bd16084a9905680f
Grigory-Vladimiro <grigori.vladimiro@gmail.com>
2025-02-06 12:24:33 +0100

    eng_translation

--- src/main/java/model/Ghost.java
+++ src/main/java/model/Ghost.java
@@ -3,9 +3,39 @@ package model;
 import java.time.LocalDate;
 
 public class Ghost {
-private String nombre;
-    private ClassGhost clase;
-    private DangerLevel nivelPeligro;
+private String name;
+    private ClassGhost class;
+    private DangerLevel dangerLevel;
     private String ability;
     private LocalDate dateBusted;
+    public String getName() {
+        return name;
+    }
+    public ClassGhost getClass() {
+        return class;
+    }
+    public DangerLevel getDangerLevel() {
+        return DangerLevel;
+    }
+    public String getAbility() {
+        return ability;
+    }
+    public LocalDate getDateBusted() {
+        return dateBusted;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setClass(ClassGhost class) {
+        this.class = class;
+    }
+    public void setDangerLevel(DangerLevel dangerLevel) {
+        this.dangerLevel = dangerLevel;
+    }
+    public void setAbility(String ability) {
+        this.ability = ability;
+    }
+    public void setDateBusted(LocalDate dateBusted) {
+        this.dateBusted = dateBusted;
+    }
 }
--- src/test/java/model/GhostTest.java
+++ src/test/java/model/GhostTest.java
@@ -8,7 +8,7 @@ public class GhostTest {
 
     @Test
     public void testCreateGhost() {
-        Ghost ghost = new Fantasma("Espíritu del Pescador de Lastres", ClaseFantasma.CLASE_IV, NivelPeligro.BAJO, "Aparecer durante tormentas en la costa");
+        Ghost ghost = new Ghost("Espíritu del Pescador de Lastres", ClaseFantasma.CLASE_IV, NivelPeligro.BAJO, "Aparecer durante tormentas en la costa");
         assertThat(ghost.getName(), is("Espíritu del Pescador de Lastres"));
         assertThat(ghost.getClass(), is(ClaseFantasma.CLASE_IV));
         assertThat(ghost.getDangerLevel(), is(NivelPeligro.BAJO));
