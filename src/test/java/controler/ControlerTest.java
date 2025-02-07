eced8452c143e88e3d54e1d7d9f7991d9dcb8862
Emagmunioz <emagmunioz@gmail.com>
2025-02-07 11:04:50 +0100

    test controler run ok

--- src/test/java/controler/ControlerTest.java
+++ src/test/java/controler/ControlerTest.java
@@ -1,21 +1,21 @@
 package controler;
 import model.Ghost;
+import model.Ghost.DangerLevel;
+import model.Ghost.GhostClass;
 import model.Player;
-import org.junit.Test;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
-
 import java.time.LocalDate;
 import java.util.List;
-@Test
+import org.junit.jupiter.api.Test;
 public class ControlerTest {
 
-    @Test
-    public void testCaptureGhost() {
-        Player player = new Player();
-        GhostController controller = new GhostController(player);
-        controller.captureGhost("Spirit of the Fisherman of Lastres", GhostClass.CLASS_IV, DangerLevel.LOW, "Appears during storms on the coast");
-        assertThat(player.getCapturedGhosts(), hasSize(1));
+@Test
+ public void testCaptureGhost() {
+    Player player = new Player();
+    Controler controler = new Controler(player);
+    controler.captureGhost("Spirit of the Fisherman of Lastres", GhostClass.CLASS_IV, DangerLevel.LOW, "Appears during storms on the coast");
+    assertThat(player.getCapturedGhosts(), hasSize(1));
     }
 
 @Test
